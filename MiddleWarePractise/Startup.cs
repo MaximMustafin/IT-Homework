49f6c06119d21c1dba24b5b2fb8e2f34f4b7151b
Maksim <maksim.mystafa@gmail.com>
2021-03-05 18:20:55 +0300

    Exception handing pages

---
+++ MiddleWarePractise/Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiddleWarePractise.CustomMiddlewares;
+
+namespace MiddleWarePractise
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<AuthenticationMiddleware>();
+            //app.Use(async (context, next) =>
+            //{
+            //    int x = 0;
+            //    int y = 8 / x;
+            //    await context.Response.WriteAsync($"Result = {y}");
+            //});
+            app.Map("/customError500", ap => ap.Run(async (context) =>
+            {
+                int[] array = { 1, 2, 3, 4, 5 };           
+                await context.Response.WriteAsync($"{array[5]}\n");          
+            }));
+            app.Map("/hello", ap => ap.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Welcome back!!!");
+            }));
+            
+        }
+    }
+}
