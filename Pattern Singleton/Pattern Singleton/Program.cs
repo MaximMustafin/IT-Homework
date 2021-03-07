d6294b26e1edd0c71ded6b2f1309f28fdbc9ca91
Maksim <maksim.mystafa@gmail.com>
2021-03-07 12:46:08 +0300

    Example of pattern Singleton (from Refactoring Guru)

---
+++ Pattern Singleton/Pattern Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            Singleton s1 = Singleton.GetInstance();
+            
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
