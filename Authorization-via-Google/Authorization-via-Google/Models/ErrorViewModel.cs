f730e83b8259431b90b9ea17cc2fc7e7c5d919e0
Maksim <maksim.mystafa@gmail.com>
2021-03-08 17:13:18 +0300

    Authorization via Google by using Asp.NetCore MVC with individual user accounts

---
+++ Authorization-via-Google/Authorization-via-Google/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authorization_via_Google.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
